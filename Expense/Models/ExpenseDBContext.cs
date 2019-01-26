c61a052ef76cf30e784b06458616917d2938ae95
Laurentiu Tordoi <laurmale@gmail.com>
2019-01-26 12:34:36 +0200

    Added all main app files

---
+++ Expense/Models/ExpenseDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expense.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:laurtor2.database.windows.net,1433;Initial Catalog=ExpenseManager;Persist Security Info=False;User ID=admindb;Password=l@urtor2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    }
+}
